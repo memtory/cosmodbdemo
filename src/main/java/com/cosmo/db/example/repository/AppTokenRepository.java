44e05ca6157685cef6d11930726c991c919b6bf3
memtory <memtory@gmail.com>
2022-03-18 13:43:45 -0400

    Update AppTokenRepository.java

--- src/main/java/com/cosmo/db/example/repository/AppTokenRepository.java
+++ src/main/java/com/cosmo/db/example/repository/AppTokenRepository.java
@@ -3,9 +3,9 @@ package com.cosmo.db.example.repository;
 import com.azure.spring.data.cosmos.repository.ReactiveCosmosRepository;
 import com.cosmo.db.example.model.entity.TokenEntity;
 import org.springframework.stereotype.Repository;
-import reactor.core.publisher.Mono;
+import reactor.core.publisher.Flux;
 
 @Repository
 public interface AppTokenRepository extends ReactiveCosmosRepository<TokenEntity, String> {
-    Mono<TokenEntity> findBySrcId(String srcId);
+    Flux<TokenEntity> findBySrcId(String srcId);
 }
