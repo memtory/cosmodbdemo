0769329e00c6eea893cdfd0303c1deb76c9f6314
memtory <memtory@gmail.com>
2022-03-18 12:06:16 -0400

    commented test

--- src/test/java/com/cosmo/db/example/CosmodbdemoApplicationTests.java
+++ src/test/java/com/cosmo/db/example/CosmodbdemoApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class CosmodbdemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+// 	@Test
+// 	void contextLoads() {
+// 	}
 
 }
