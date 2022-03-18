54d6e67e8487a59854cdd3e723fb181dd8ca6f33
memtory <memtory@gmail.com>
2022-03-18 14:45:18 -0400

    Update AppTokenService.java

--- src/main/java/com/cosmo/db/example/service/AppTokenService.java
+++ src/main/java/com/cosmo/db/example/service/AppTokenService.java
@@ -5,10 +5,7 @@ import com.cosmo.db.example.model.entity.TokenEntity;
 import com.cosmo.db.example.repository.AppTokenRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-
-import java.util.List;
 
 @Service
 public class AppTokenService {
@@ -18,9 +15,8 @@ public class AppTokenService {
 
     public TokenModel getTokenBySrcId(String srcId) {
         TokenModel tokenModel =  new TokenModel();
-        Flux<TokenEntity> findBySrcId = appTokenRepository.findBySrcId(srcId);
-        List<TokenEntity> entityList = findBySrcId.collectList().block();
-        TokenEntity entity = entityList.get(0);
+        Mono<TokenEntity> findBySrcId = appTokenRepository.findBySrcId(srcId);
+        TokenEntity entity = findBySrcId.block();
         tokenModel.setId(entity.getId());
         tokenModel.setEmailId(entity.getEmailId());
         tokenModel.setSrcId(entity.getSrcId());
